56d9f7b9754bf8eb80c07377346a86889bd14fea
PJ568 <PJ568@PJ568.eu.org>
2023-09-11 09:49:52 +0800

    新增下载页面和国际化支持

---
+++ js/translate_lib.js
@@ -0,0 +1,10 @@
+try{
+    translate.listener.start();
+    translate.language.setLocal('chinese_simplified');
+    translate.setAutoDiscriminateLocalLanguage();
+    translate.language.setUrlParamControl();
+    translate.ignore.class.push('notTranslate');
+}
+catch(e){console.log(e);}
+translate.setUseVersion2();
+translate.execute();
