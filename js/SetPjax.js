c2c21ad8f3bd84c2d186a79da587846571102d2c
PJ568 <PJ568@PJ568.eu.org>
2024-03-27 14:33:12 +0800

    [新增] Pjax支持

---
+++ js/SetPjax.js
@@ -0,0 +1,15 @@
+var pjax = new Pjax({
+    selectors: [
+        "head meta",
+        "head title",
+        ".pjax-reload"
+    ],
+});
+
+// function pjax_reload() {
+//     collapsible();
+// }
+
+// document.addEventListener("pjax:complete", function () {
+//     pjax_reload();
+// });
